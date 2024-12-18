f1b413a1aea6fe618af7d3c3a97a596cd1763edd
Aroslav Kochkin <kochkin.2328@inbox.ru>
2024-12-18 12:05:35 +0700

    correct-03

---
+++ BookStoreApp/ViewControllers/MulteipleSectionsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MulteipleSectionsViewController.swift
+//  BookStoreApp
+//
+//  Created by Ярослав Кочкин on 18.12.2024.
+//
+
+import UIKit
+
+class MulteipleSectionViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
